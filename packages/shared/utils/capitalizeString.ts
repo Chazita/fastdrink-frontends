4dd86c6506067eca1f344cbcd732345f10f9f540
Chazita <chazarreta.matias.1138@gmail.com>
2022-06-10 20:11:44 -0300

    add , updates types & utils

--- packages/shared/utils/capitalizeString.ts
+++ packages/shared/utils/capitalizeString.ts
@@ -1,10 +1,12 @@
-export const capitalizeString = (word: string): string => {
+function capitalizeString(word: string): string {
 	return word.replace(/(?:^|\s)\S/g, (res) => {
 		return res.toUpperCase();
 	});
-};
+}
 
-export const capitalizeStringUnderscore = (word: string): string => {
+function capitalizeStringUnderscore(word: string): string {
 	const wordNoUnderscore = word.replace("_", " ");
 	return capitalizeString(wordNoUnderscore);
-};
+}
+
+export { capitalizeString, capitalizeStringUnderscore };
