abbad6cf94089f4f28345569314a9e37f91c6583
Chazita <chazarreta.matias.1138@gmail.com>
2022-07-06 21:07:19 -0300

    add configuration

--- apps/web-admin/src/pages/user/configuration/index.tsx
+++ apps/web-admin/src/pages/user/configuration/index.tsx
@@ -1,17 +1,21 @@
 import { Divider, Flex } from "@chakra-ui/react";
+import UserContext from "contexts/userContext";
+import { useContext } from "react";
 import {
 	ChangeName,
 	ChangePassword,
 	ChangeEmailAddress,
-} from "components/User/Configuration";
+} from "ui/src/User/Configuration";
+
 const UserConfiguration = () => {
+	const { userInfo, userRefetch } = useContext(UserContext);
 	return (
 		<Flex direction={"column"}>
-			<ChangeName />
+			<ChangeName userInfo={userInfo} userRefetch={userRefetch} />
 			<Divider mt={"5"} mb={"5"} />
-			<ChangePassword />
+			<ChangePassword userRefetch={userRefetch} />
 			<Divider mt={"5"} mb={"5"} />
-			<ChangeEmailAddress />
+			<ChangeEmailAddress userInfo={userInfo} userRefetch={userRefetch} />
 			<Divider mt={"5"} mb={"5"} />
 		</Flex>
 	);
--- apps/web/src/pages/user/configuration/index.tsx
+++ apps/web/src/pages/user/configuration/index.tsx
@@ -1,5 +1,24 @@
+import { Divider, Flex } from "@chakra-ui/react";
+import { UserContext } from "contexts/userContext";
+import { useContext } from "react";
+import {
+	ChangeName,
+	ChangePassword,
+	ChangeEmailAddress,
+} from "ui/src/User/Configuration";
+
 const UserConfiguration = () => {
-	return <></>;
+	const { userInfo, userRefetch } = useContext(UserContext);
+	return (
+		<Flex ml={"2"} mr="2" direction={"column"}>
+			<ChangeName userInfo={userInfo} userRefetch={userRefetch} />
+			<Divider mt={"5"} mb={"5"} />
+			<ChangePassword userRefetch={userRefetch} />
+			<Divider mt={"5"} mb={"5"} />
+			<ChangeEmailAddress userInfo={userInfo} userRefetch={userRefetch} />
+			<Divider mt={"5"} mb={"5"} />
+		</Flex>
+	);
 };
 
 export default UserConfiguration;
