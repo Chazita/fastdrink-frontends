b9ab08748722644fcf1689feedf9f06732d6cfa8
Chazita <chazarreta.matias.1138@gmail.com>
2022-07-11 19:13:04 -0300

    env changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,7 +4,4 @@ const withTM = require("next-transpile-modules")(["ui", "shared"]);
  */
 module.exports = withTM({
 	reactStrictMode: true,
-	env: {
-		API_URL: "https://localhost:7134/api",
-	},
 });
