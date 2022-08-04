f11521594681f6e6b36fceb0d6bd090d3b5ac9cc
Chazita <chazarreta.matias.1138@gmail.com>
2022-08-04 19:29:11 -0300

    content is added

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,58 @@
+import {
+	Box,
+	Flex,
+	Heading,
+	HStack,
+	SimpleGrid,
+	Stack,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { ProductCard } from "components/Products";
+import { useQuery } from "react-query";
+import { Product } from "shared";
+
 const Home = () => {
-	return <>No se me ocurre algun diseño {":("}</>;
+	const newProducts = useQuery("new-products", () => {
+		return axios.get<Product[]>("/Product/new-products");
+	});
+
+	const discountsProducts = useQuery("discount-products", () => {
+		return axios.get<Product[]>("/Product/new-discounts-products");
+	});
+
+	if (
+		discountsProducts.isLoading &&
+		newProducts.isLoading &&
+		newProducts.data === undefined
+	) {
+		return <></>;
+	}
+
+	return (
+		<div>
+			<Box padding={5}>
+				<Heading mb={4}>Nuevos Products</Heading>
+				<SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={8}>
+					{newProducts.data.data.map((product) => (
+						<ProductCard item={product} key={product.id} />
+					))}
+				</SimpleGrid>
+			</Box>
+
+			<Box padding={5}>
+				<Heading mb={4}>Nuevos Descuentos</Heading>
+				<SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={8}>
+					{discountsProducts.data !== undefined ? (
+						discountsProducts.data.data.map((product) => (
+							<ProductCard item={product} key={product.id} />
+						))
+					) : (
+						<></>
+					)}
+				</SimpleGrid>
+			</Box>
+		</div>
+	);
 };
 
 export default Home;
